fe5a36c42ce60471a27820b767bb0ed20cb21934
TzeWey <me@hion.tw>
2025-01-13 17:10:52 +0000

    Load 'nct7802' module

--- fs68-pre-init.py
+++ fs68-pre-init.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
+import subprocess
 from fs68 import FS68, OutputType, OutputMode, PowerMode
+
+
+def modprobe(module: str):
+    if subprocess.call(["modprobe", module]) != 0:
+        print(f"> Failed to load module: {module}")
+    else:
+        print(f"> Loaded module: {module}")
+
+
+modprobe("nct7802")
 
 
 with FS68() as fs:
